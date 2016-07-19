372611d40c4c8033c57f4b2c84f0b66f6fa6a7d7
Satoshi Sahara <sahara.satoshi@gmail.com>
2016-07-19 22:55:54 +0900

    show php error_log filename in config

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -8,4 +8,4 @@
 $lang['toolbar_button']     = 'enable toolbar button';
 $lang['use_cstyle_nest']    = 'use nested C-style comments syntax';
 $lang['use_oneline_style']  = 'use one-line style comments syntax';
-$lang['log_invalid_macro']  = 'record invalid macro directives in PHP error log file';
+$lang['log_invalid_macro']  = 'record invalid macro directives in PHP error log: '.hsc(ini_get('error_log'));
