a443764c788ea569fcc49b781d8ec5c183a8eb57
Satoshi Sahara <sahara.satoshi@gmail.com>
2018-06-14 20:31:43 +0900

    add note for one-line comment syntax
    Apparently, the one-line comment syntax may interfere with the markup for italics!

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -7,5 +7,6 @@
 
 $lang['toolbar_button']     = 'enable toolbar button';
 $lang['use_cstyle_nest']    = 'use nested C-style comments syntax';
-$lang['use_oneline_style']  = 'use one-line style comments syntax';
+$lang['use_oneline_style']  = 'use one-line style comments syntax //<br>'
+                             .'(note: this may interfere with the markup for italics)';
 $lang['log_invalid_macro']  = 'record invalid macro directives in PHP error log: '.hsc(ini_get('error_log'));
--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -7,4 +7,5 @@
 
 $lang['toolbar_button']     = 'ツールバーにコメント化ボタンを表示する';
 $lang['use_cstyle_nest']    = '入れ子の C-style コメント記法を使用する';
-$lang['use_oneline_style']  = '1行コメント記法を使用する';
+$lang['use_oneline_style']  = '1行コメント記法 // を使用する<br>'
+                             .'（※斜体文字への整形は行をまたげないという制限が生じます）';
