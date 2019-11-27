23f0177461cdbf5b6a69003b9b24bb3bc401bb9e
Щербинин Андрей Владимирович <ScherbininAV@hq.corp.mos.ru>
2019-11-27 15:52:33 +0300

    Russian Localization
     * Added files for russian localization

---
+++ lang/ru/lang.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Comment Syntax plugin for DokuWiki
+ * Russian Language file
+ * @author  Intervision https://github.com/intervisionlord
+ */
+$lang['toolbar_title'] = 'Закомментировать выделенный текст';
---
+++ lang/ru/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Comment Syntax plugin for DokuWiki
+ * Russian Language for settings
+ * @author  Intervision https://github.com/intervisionlord
+ */
+
+$lang['toolbar_button']     = 'Добавить кнопку в тулбар';
+$lang['use_cstyle_nest']    = 'Использовать вложенный синтаксис в стиле C';
+$lang['use_oneline_style']  = 'Использовать синтаксис однострочных комментариев //<br>'
+                             .'(винмание: синтаксис может пересекаться с синтаксисом курсива)';
+$lang['log_invalid_macro']  = 'Записывать некорректные вызовы в лог ошибок PHP error log: '.hsc(ini_get('error_log'));
